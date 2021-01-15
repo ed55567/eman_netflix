0b6387bd3ff9539d8d248129cac5be67ec0d1c3b
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2021-01-14 23:54:20 -0500

    added global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { GlobalStyles } from './global-styles';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<>
+  <GlobalStyles />
+  <App />
+</>,
+  document.getElementById('root'));
 
 
